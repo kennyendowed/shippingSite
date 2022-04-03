2a9f18500dc31dd350e42cbe68f5660e27a6e183
kenneth akpan <kenneyg50@gmail.com>
2022-04-03 16:50:03 +0100

    secondCommint

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,36 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  },
+  agora: {
+    appId: '633313442061443482e167550875b1e8',
+    codec: 'h264',
+    mode:'rtc',
+    role:'host'
+
+  },
+   secretKey: 'MIICXAIBAAKBgQDfmlc2EgrdhvakQApmLCDOgP0nNERInBheMh7J/r5aU8PUAIpGXET/8',
+   API_REQ_KEY:'9O4aVEzgX55ZMefIrJ5vkJzw7kmWaZaa7nHuQRLusdyNlH6PrZpjdAnLFwJmLbOJ',
+   url:'http://127.0.0.1:8000/api/',
+  STORAGE_REQ_KEY: 'storedreq',
+  deeplink_url: "",
+  jsFiles: [
+  ],
+  cssFiles: [
+    ''
+  ],
+  flutterwave_config: {
+    url: "",
+    callback: "",
+    api_key: "",
+    api_secret: "",
+    public_key: ""
+  }
+}
